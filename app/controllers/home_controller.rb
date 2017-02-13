2dfc388d3945f1b9acc540264d957052b80c26a1
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-02-13 18:55:50 +0200

    Displaying images in thumbnails, also can press on them goes to source

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,16 @@ class HomeController < ApplicationController
   before_action :load_api_keys
 
   def index
-    @photo_links = []
-
   end
 
   def search
-    @search = params[:search]
-    @photo_links = []
-    photos = flickr.photos.search(text: @search, per_page: 5, page: 1, extras: 'url_c')
-    photos.each do |photo|
-      @photo_links << photo['url_c']
+    if params[:search]
+      search = params[:search]
+      @photo_links = []
+      photos = flickr.photos.search(text: search, per_page: 6, page: 1, extras: 'url_m')
+      photos.each do |photo|
+        @photo_links << photo['url_m']
+      end
     end
     render :index
   end
