a830068c79aa69ae52ca148933ed192a9f62d2f8
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-02-13 17:48:59 +0200

    Getting photos urls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,11 @@ class HomeController < ApplicationController
   before_action :load_api_keys
 
   def index
-
-    @photos = flickr.photos.search(text: 'beach', per_page: 5, page: 1)
-
+    @photo_links = []
+    photos = flickr.photos.search(text: 'beach', per_page: 5, page: 1, extras: 'url_c')
+    photos.each do |photo|
+      @photo_links << photo['url_c']
+    end
   end
 
   def search
