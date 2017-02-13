69e2ece6a0cc5a53e47eecf65777218a268abcf6
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-02-13 17:34:31 +0200

    Getting json response from flickr.photo.search api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
+  before_action :load_api_keys
+
   def index
+
+    @photos = flickr.photos.search(text: 'beach', per_page: 5, page: 1)
+
   end
 
   def search
+
   end
+
+  private
+
+  def load_api_keys
+    FlickRaw.api_key = ENV['FLICKER_KEY']
+    FlickRaw.shared_secret = ENV['FLICKER_SECRET']
+  end
+
 end
