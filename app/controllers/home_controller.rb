511c4ba5fa2b052b632ffe7e7559b8c8cebc3779
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-02-15 13:17:43 +0200

    Increasing photo count per search to 10, adding chages to coresponding tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   def search
     params[:search].present? ? search = params[:search] : search = "dogs"
     @photo_links = []
-    photos = flickr.photos.search(text: search, per_page: 6, page: 1, extras: 'url_z')
+    photos = flickr.photos.search(text: search, per_page: 10, page: 1, extras: 'url_z')
     photos.each do |photo|
       @photo_links << photo['url_z']
     end
